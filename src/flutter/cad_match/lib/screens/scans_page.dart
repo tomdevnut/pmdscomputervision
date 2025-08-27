9fda1515570066730bb0eeb1fa27d6123158d29e
Vittorio Sironi <vittorio.sironi@icloud.com>
2025-08-27 18:17:22 +0200

    Risoluzione problema build iOS

--- src/flutter/cad_match/lib/screens/scans_page.dart
+++ src/flutter/cad_match/lib/screens/scans_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'scanning_page.dart';
 
 class ScansPage extends StatefulWidget {
   const ScansPage({super.key});
