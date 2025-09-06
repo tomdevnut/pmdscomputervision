d28e077ad5f2a5be74bdf0a9465eca6bde357377
Vittorio Sironi <vittorio.sironi@icloud.com>
2025-09-06 12:19:08 +0200

    Aggiorna i percorsi di Flutter per l'ambiente di sviluppo e aggiungi la sezione degli asset nel pubspec.yaml

--- src/flutter/cad_match/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ src/flutter/cad_match/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\stefa\Downloads\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\stefa\Progetti\PolimiXdevnut\pmdscomputervision\src\flutter\cad_match"
+export "FLUTTER_ROOT=/Users/vittoriosironi/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/vittoriosironi/DEVNUT/client/pmdscomputervision/src/flutter/cad_match"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
